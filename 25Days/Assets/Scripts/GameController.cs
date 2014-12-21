9688aef7abc0b3c30cfd03cfbde6850dbea4bbdb
rainbee2214 <rainbee2214@gmail.com>
2014-12-21 00:01:28 -0400

    Basic setup

---
+++ 25Days/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+    public static GameController controller;
+
+    void Awake()
+    {
+        if (controller == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller = this;
+        }
+        else if (controller != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 25Days/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour 
+{
+    public float delay = 2f;
+    public string level;
+
+	void Update () 
+    {
+	    if (Time.timeSinceLevelLoad > delay)
+        {
+            Application.LoadLevel(level);
+        }
+	}
+}
